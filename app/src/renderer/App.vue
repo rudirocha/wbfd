8ea528f9a0384e4f5c06ae348a29b5905fb7075d
Rudi Rocha <rudi.rocha@gmail.com>
2017-04-10 00:03:49 +0100

    Initial commit

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="#app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+</style>
---
+++ app/src/renderer/components/WebBrowserView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-2">
+        <aside class="menu">
+  <p class="menu-label">
+    Available links
+  </p>
+  <ul class="menu-list">
+    <li><a> <i class="fa fa-github"></i>GitHub</a></li>
+    <li><a>Customers</a></li>
+  </ul>
+</aside>
+      </div>
+        <div class="column">
+col 2
+        </div>
+          <div class="column">
+col 3
+          </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
