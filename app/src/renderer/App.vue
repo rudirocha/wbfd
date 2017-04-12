7daff18e44996759816cfe1998a01d0892902d62
Rudi Rocha <rudi.rocha@gmail.com>
2017-04-12 23:03:20 +0100

    Add new Stuff

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="#app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import store from 'renderer/vuex/store'
+  export default {
+    store
+  }
+</script>
+
+<style>
+  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+</style>
